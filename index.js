d0db20e2087a1541f47161c5d65391a60be6a83f
Jonathan K <82918934+noudas@users.noreply.github.com>
2024-06-18 13:30:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+const cr2Raw = require("cr2-raw");
+
+const inputFolder = "D:/Fotos/Israel Sharei 2023/Raw/HUJI + Yafo";
+const outputFolder = "D:/Fotos/Israel Sharei 2023/PNG/HUJI + Yafo";
+
+// Read the input folder and convert all CR2 files to PNG
+fs.readdir(inputFolder, (err, files) => {
+  if (err) {
+    console.error(`Error reading the input folder: ${err.message}`);
+  } else {
+    const cr2Files = files.filter(
+      (file) => path.extname(file).toLowerCase() === ".cr2"
+    );
+    cr2Files.forEach((cr2File) => {
+      const cr2FilePath = path.join(inputFolder, cr2File);
+      console.log("Processing:", cr2FilePath);
+      convertCR2toPNG(cr2FilePath);
+    });
+  }
+});
+
+const convertCR2toPNG = (cr2FilePath) => {
+  const outputFileName = path.basename(cr2FilePath, path.extname(cr2FilePath)) + ".png";
+  var raw = cr2Raw(cr2FilePath);
+  fs.writeFileSync(`${outputFolder}/${outputFileName}`, raw.previewImage());
+};
